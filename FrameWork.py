ecca1d62d280d09bf5f27acc305cfcdf8e8d04f1
Julia Harvie <julia.harvie1212@gmail.com>
2020-12-06 03:20:48 -0500

    Early early sunday

--- FrameWork.py
+++ FrameWork.py
@@ -1,59 +1,61 @@
 from PIL import Image
 import time
+import glob
 import numpy as np
 import cv2
 
 # Start here
-im2 = cv2.imread("/Users/julia/Desktop/CIS6320/SegmentationAlgorthimImages/BIOUG53847_78_G06.jpg")
-# Explore it
-print("im2 ndim: ", im2.ndim)
-print("im2 shape:", im2.shape)
-print("im2 size: ", im2.size)
-print("im2 dtype:", im2.dtype)
-
-# cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-# cv2.imshow('image', im2)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-LUV = cv2.cvtColor(im2, cv2.COLOR_RGB2Luv)
-print("luv ndim: ", LUV.ndim)
-print("luv shape:", LUV.shape)
-print("luv size: ", LUV.size)
-print("luv dtype:", LUV.dtype)
-
-# Create an array to store stuff in
-
-# # Define thresholds:
-# Lmax = 100
-# Umax = 100
-# Vmax = 100
+files = glob.glob("/Users/julia/Desktop/CIS6320/SegmentationAlgorthimImages/*")
+im2 = cv2.imread(files[0])
+# # Explore it
+# print("im2 ndim: ", im2.ndim)
+# print("im2 shape:", im2.shape)
+# print("im2 size: ", im2.size)
+# print("im2 dtype:", im2.dtype)
 #
-# # Loop test
-# A = time.perf_counter()
-# mask = np.random.choice([0], size=(2160, 2880))
-# B = time.perf_counter()
-# for i in im2:
-#     mask[i] = 10
-# C = time.perf_counter()
-# print(mask.shape, mask.size, mask[:6])
-# print(B-A, C-B)
-
-mask = np.random.choice([0], size=(200, 150))
-
-num = LUV[:200, :150, 0]
-print(max(np.nditer(num)), min(np.nditer(num)))
-for i in range(0, 200):
-    for j in range(0, 150):
-        if num[i, j] <= 230:
-            mask[i, j] = 1
-print(im2[:10,:10, 0])
-print(LUV[:10,:10, 0])
-
-# cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-# cv2.imshow('image', LUV)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# # cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+# # cv2.imshow('image', im2)
+# # cv2.waitKey(0)
+# # cv2.destroyAllWindows()
+# LUV = cv2.cvtColor(im2, cv2.COLOR_RGB2Luv)
+# print("luv ndim: ", LUV.ndim)
+# print("luv shape:", LUV.shape)
+# print("luv size: ", LUV.size)
+# print("luv dtype:", LUV.dtype)
 #
+# # Create an array to store stuff in
+#
+# # # Define thresholds:
+# # Lmax = 100
+# # Umax = 100
+# # Vmax = 100
+# #
+# # # Loop test
+# # A = time.perf_counter()
+# # mask = np.random.choice([0], size=(2160, 2880))
+# # B = time.perf_counter()
+# # for i in im2:
+# #     mask[i] = 10
+# # C = time.perf_counter()
+# # print(mask.shape, mask.size, mask[:6])
+# # print(B-A, C-B)
+#
+# mask = np.random.choice([0], size=(200, 150))
+#
+# num = LUV[:200, :150, 0]
+# print(max(np.nditer(num)), min(np.nditer(num)))
+# for i in range(0, 200):
+#     for j in range(0, 150):
+#         if num[i, j] <= 230:
+#             mask[i, j] = 1
+# print(im2[:10,:10, 0])
+# print(LUV[:10,:10, 0])
+#
+# # cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+# # cv2.imshow('image', LUV)
+# # cv2.waitKey(0)
+# # cv2.destroyAllWindows()
+# #
 
 # Convert BGR to HSV
 luv = cv2.cvtColor(im2, cv2.COLOR_RGB2Luv)
@@ -68,6 +70,7 @@ mask = cv2.inRange(luv, lower, upper)
 # Bitwise-AND mask and original image
 Final = cv2.bitwise_and(im2, im2, mask=mask)
 
+
 cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
 cv2.namedWindow('Mask', cv2.WINDOW_NORMAL)
 cv2.namedWindow('Final', cv2.WINDOW_NORMAL)
@@ -75,5 +78,5 @@ cv2.imshow('Original', im2)
 cv2.imshow('Mask', mask)
 cv2.imshow('Final', Final)
 cv2.waitKey(0)
-
-cv2.bitwise_and()
+#
+# cv2.bitwise_and()
--- ThresholdMasks.py
+++ ThresholdMasks.py
@@ -6,22 +6,52 @@ import cv2
 
 # Read in the file path for every image
 files = glob.glob("/Users/julia/Desktop/CIS6320/SegmentationAlgorthimImages/*")
-# Now I need to store them in a place that will also allow me to attach threshold values
-AllImage = {
-    "image1": files[0]
-}
-print(AllImage["image1"])
+# Currently have to enter threshold values by hand based off extracting them from imageJ
+# Note these are in lab not luv
+MeasuredThresholds = np.array([
+    [[0, 0, 139], [196, 255, 255]],  # TH1, low, high
+    [[0, 0, 127], [204, 255, 255]],  # TH2, low, high
+    [[0, 0, 124], [162, 255, 255]]])  # TH3, low, high
+given = MeasuredThresholds.shape[2]
+# Calculate low limit and upper limit for each channel by averaging the manually extracted values
+AveRange = np.array([[np.mean(MeasuredThresholds[:given, 0, 0]),  # low, l u v
+                      np.mean(MeasuredThresholds[:given, 0, 1]),
+                      np.mean(MeasuredThresholds[:given, 0, 2])],
+                     [np.mean(MeasuredThresholds[:given, 1, 0]),  # high, l u v
+                      np.mean(MeasuredThresholds[:given, 1, 1]),
+                      np.mean(MeasuredThresholds[:given, 1, 2])]], dtype="int")
+# Calculate low limit and upper limit for each channel by choice the values that encompass every extracted value
+WideRange = np.array([[min(MeasuredThresholds[:given, 0, 0]),  # low, l u v
+                       min(MeasuredThresholds[:given, 0, 1]),
+                       min(MeasuredThresholds[:given, 0, 2])],
+                      [max(MeasuredThresholds[:given, 1, 0]),  # high, l u v
+                       max(MeasuredThresholds[:given, 1, 1]),
+                       max(MeasuredThresholds[:given, 1, 2])]], dtype="int")
 
-# #This function works on one image at a time using values for l, u and v calculated about
-# def Threshold(InputImg, l, u, v):
-#     # Convert Input Image from RGB to luv
-#     LUV = cv2.cvtColor(InputImg, cv2.COLOR_RGB2Luv)
-#     # Check the bite type. Expecting 8 bite for all but just in case
-#     if LUV.dtype == "uint8":
-# # If this is true then open cv has scaled the pixels from luv colour shape range to 0:255
-#
-#
-#
-# # This version involves a single import of every image
-# img = cv2.imread("/Users/julia/Desktop/CIS6320/SegmentationAlgorthimImages/BIOUG53847_78_G06.jpg")
-# luvThreshold(img)
+
+# This function works on one image at a time using values for l, u and v calculated about
+def Threshold(InputImg, Range):
+    # Check the bite type. Expecting 8 bite for all but just in case
+    if InputImg.dtype != "uint8":
+        print("not 8 bite: " + InputImg.dtype)
+    # Convert Input Image from RGB to luv
+    Luv = cv2.cvtColor(InputImg, cv2.COLOR_RGB2Luv)
+    # Define the mask
+    mask = cv2.inRange(Luv, Range[0], Range[1])
+    # Apply mask to original image
+    segmented = cv2.bitwise_and(InputImg, InputImg, mask=mask)
+    return mask, segmented
+
+
+def Display(Original, Threshold):
+    cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('Mask', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('Segmented', cv2.WINDOW_NORMAL)
+    cv2.imshow('Original', Original)
+    cv2.imshow('Mask', Threshold[0])
+    cv2.imshow('Segmented', Threshold[1])
+    cv2.waitKey(0)
+
+
+One = Threshold(cv2.imread(files[0]), WideRange)
+Display(cv2.imread(files[0]), One)
